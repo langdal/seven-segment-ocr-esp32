8fcbacb1fba86ecd95023ece3b262efea4e59cb4
Jakob Langdal <jakob@langdal.dk>
2024-08-15 19:11:35 +0200

    Add library

--- components/seven_segment_ocr/text_sensor.py
+++ components/seven_segment_ocr/text_sensor.py
@@ -11,7 +11,7 @@ CONFIG_SCHEMA = text_sensor.text_sensor_schema(SevenSegmentOCR).extend(
     cv.COMPONENT_SCHEMA
 )
 
-
 async def to_code(config):
     var = await text_sensor.new_text_sensor(config)
     await cg.register_component(var, config)
+    await cg.add_library("vshymanskyy/TinyGSM@0.12.0")
