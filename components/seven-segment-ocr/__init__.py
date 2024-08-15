418ec64bf13821dc64151934fd1e8500d3f886a9
Jakob Langdal <jakob@langdal.dk>
2024-08-15 18:43:42 +0200

    Update namespace

--- components/seven-segment-ocr/__init__.py
+++ components/seven-segment-ocr/__init__.py
@@ -2,7 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID, CONF_LAMBDA, CONF_TEXT_SENSORS
-from .. import custom_ns
+import esphome.codegen as cg
+custom_ns = cg.esphome_ns.namespace("custom")
 
 SevenSegmentOCRConstructor = custom_ns.class_("SevenSegmentOCRConstructor")
 
